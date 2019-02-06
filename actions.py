0a1982d5db375e89a15015e0381b2ce66df2abdf
Nadya <nadeia.ak@gmail.com>
2019-02-06 11:10:37 +0300

    nadya

--- actions.py
+++ actions.py
@@ -2,35 +2,90 @@
 from data import  population, clubs
 from components import Club, Person
 
-my_name = ""
-my_age = -1
-my_bio = ""
+my_name = "Nadya"
+my_age = 23
+my_bio = " dkks "
 myself = Person(my_name, my_bio, my_age)
 
 def introduction():
     print("Hello, %s. Welcome to our portal." % my_name)
 
 def options():
-    # your code goes here!
+    while True:
+        print("------------------------")
+        print("Would you like to:")
+        print("1) Creat a new club .\n or:")
+        print("2) Browse and join_clubs .\n or:")
+        print("3) View existing clubs .\n or:")
+        print("4) Display members of a club .\n or:")
+        print("-1) Close application .")
+        choose = input()
+        if choose == "1":
+            create_club()
+        elif choose == "2":
+            join_clubs()
+        elif choose == "3":
+            view_clubs()
+        elif choose == "4":
+            view_club_members()
+        elif choose =="-1":
+            break
+        else :
+            print(" Wrong number try agein")    
+        
+
     
 
 def create_club():
     # your code goes here!
-    
+    club_name = input("Pick a name for your awesome new club :")
+    club_des = input("What id your club about?")
+    new_club = Club(club_name,club_des)
+    print("Enter the numbers of the people you would like to recruit to your new club (-1 to stop)")
+    for index , member in enumerate (population) :
+        print ("[%d] %s" %( index+1 , member.name ))
+    club_member = int(input())
+    new_club.recruit_member(myself)
+    new_club.assign_president(myself)
+    stop = True
+    while stop :
+        if len(population) >= club_member and club_member > 0:
+            new_club.recruit_member(population[club_member -1])
+            club_member = int(input())  
+        else :
+            stop = False     
+    clubs.append(new_club)
+   
+    print ("Here\'s your club %s\n%s\n"%(new_club.name,new_club.description))
+    new_club.print_member_list()
+            
+
+
 
 def view_clubs():
-    # your code goes here!
+    for club_d in clubs :
+        p = len(club_d.members)
+        print ("\t\t\t NAME: %s\n\t\t\tDESCRIPTION: %s\n\t\t\t MEMBERS: %s \n" %(club_d.name,club_d.description, str(p)))
     
 
 def view_club_members():
-    # your code goes here!
+    view_clubs()
+    print("Enter the name of the club whose members you'd like to see : ")
+    club_cho = input()
+    for club_f in clubs :
+        if club_f.name.lower() == club_cho.lower():
+            club_f.print_member_list()
     
-
 def join_clubs():
-    # your code goes here!
-    
+    view_clubs()
+    club_n = input("Enter the name of the club you'd like to join : ")
+    for club_f in clubs :
+        if club_f.name.lower() == club_n.lower():
+            club_f.recruit_member(myself)
+            print("%s just joined %s" %(myself.name,club_n))
+            break
 
 def application():
     introduction()
-    # your code goes here!
+    options()
     
--- components.py
+++ components.py
@@ -2,21 +2,50 @@
 class Person():
     def __init__(self, name, bio, age):
         # your code goes here!
+        self.name = name
+        self.bio = bio
+        self.age = age
 
 
 class Club():
     def __init__(self, name, description):
         # your code goes here!
+        self.name = name
+        self.description = description
+        self.president = None
+        self.members = []
 
 
     def assign_president(self, person):
         # your code goes here!
-
+        self.president = person
 
     def recruit_member(self, person):
         # your code goes here!
+        self.members.append(person)
 
 
     def print_member_list(self):
         # your code goes here!
+        print ("Members: ")
+        total = 0.0
+        averg = 0.0
+        for member in self.members:
+            taxt = "- " + member.name + " (" + str(member.age) + " yeears old " 
+            taxt += ", president) " if member == self.president else ") "
+            taxt +=  member.bio
+            print(taxt)
+            total = total+ member.age    
+        lent = float(len(self.members)) 
+        averg = total /(lent)
+        print ("Average age in this club: "+ str(averg) +" yr") 
+        
+        """
+            total += member.age 
+        lent = len(self.members)+1 """ 
+
+
+
+
+
 
